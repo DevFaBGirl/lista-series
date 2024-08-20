3335aea08c4c6a467292928453a6eafbe2ea65e0
DevFabGirl <107576975+DevFaBGirl@users.noreply.github.com>
2024-08-19 23:28:42 -0300

    retirei os comentários

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const router = express.Router(); // Definindo um único router para ser reutilizado
+const router = express.Router(); 
 const cors = require('cors');
 const listaSeries = require('./dados.json'); 
 
 const app = express();
 app.use(cors());
-app.use(express.json()); // Necessário para processar o body das requisições POST
+app.use(express.json()); 
 
 //GET
 router.get('/series', (req, res) => {
@@ -13,7 +13,7 @@ router.get('/series', (req, res) => {
 });
 
 //POST
-router.post('/series', (req, res) => { // Usar router para definir a rota
+router.post('/series', (req, res) => {
     const serie = {
         id: req.body.id,
         title: req.body.title,
@@ -21,8 +21,8 @@ router.post('/series', (req, res) => { // Usar router para definir a rota
         status: req.body.status
     };
     
-    listaSeries.push(series); // Adiciona a nova série ao array original
-    res.status(201).json({message: `Série '${serie.title}' adicionada com sucesso!`}); // Retorna a nova série criada
+    listaSeries.push(series); 
+    res.status(201).json({message: `Série '${serie.title}' adicionada com sucesso!`}); 
 });
 
 
